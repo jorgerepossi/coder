cab6eb31ab51432b8219cbbec7f4f18073a7bfd8
jo Repossi <jorgerepossi1980@gmail.com>
2021-05-19 16:10:43 -0300

    producto styled

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,7 @@
+let currentValue = 0;
+let defaultValue = currentValue;
+$("#quantity__number").html(currentValue);
+
 jQuery(".scroll_to").click(function (e) {
   var jump = $(this).attr("href");
   var new_position = $(jump).offset();
@@ -21,4 +25,16 @@ jQuery("#inoRange").on(" input change", function () {
   $("#value").html(
     "$" + String($("#inoRange").val()).replace(/(.)(?=(\d{3})+$)/g, "$1.")
   );
+});
+
+jQuery(".buttons_plus").bind("click", () => {
+  $("#quantity__number").html(++defaultValue);
+});
+
+jQuery(".buttons_minus").bind("click", () => {
+  if (defaultValue != 0) {
+    $("#quantity__number").text(--defaultValue);
+  } else {
+    $("#quantity__number").text(0);
+  }
 });
